d7de41401fdd971bd7d11e8d29578fca65846454
Fagun06 <mdfagun1357@gmail.com>
2024-10-08 09:49:06 +0600

    Add project files.

---
+++ Data/InventoryDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Inventory_System_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory_System_MVC.Data
+{
+
+    public class InventoryDbContext : DbContext
+		{
+			public InventoryDbContext(DbContextOptions options) : base(options)
+			{
+
+			}
+			public DbSet<Register> Register { get; set; }
+			public DbSet<Equipment> Equipment { get; set; }
+			public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Setting up the relationship between Customer and Equipment
+            modelBuilder.Entity<Customer>()
+                .HasOne(c => c.Equipment)
+                .WithMany(e => e.Customers)
+                .HasForeignKey(c => c.EquipmentID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+	
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Inventory_System_MVC.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<InventoryDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("InventoryConnectionString")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Account}/{action=Login}/{id?}");
+
+app.Run();
